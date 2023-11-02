890699a7b846fdb57d8d4882a92bb88e0bc4a7cf
Rium <kinglessqueenrium@gmail.com>
2023-11-02 21:26:59 +0100

    added common functions.

---
+++ e4/functions.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from ..e4 import XML_Tag, XML_Fragment, XML_FragmentType
+
+from typing import Callable
+
+
+def append_child(parent: XML_Tag, child: XML_Tag):
+    insert_child(parent, len(parent.fragments), child)
+
+
+def insert_child(parent: XML_Tag, index: int, child: XML_Tag):
+    parent.fragments.insert(index, XML_Fragment(kind=XML_FragmentType.ELEMENT, data=child))
+
+
+def find_first(node: XML_Tag, condition: Callable[[XML_Fragment], bool]) -> XML_Fragment:
+    first, _ = find_first_with_index(node, condition)
+    return first
+
+
+def find_first_with_index(node: XML_Tag, condition: Callable[[XML_Fragment], bool]) -> tuple[XML_Fragment, int]:
+    for index, fragment in enumerate(node.fragments):
+        if condition(fragment):
+            return fragment, index
